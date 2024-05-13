e783623b4e5fb1c66a8f8395a141689adb3390f7
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-13 15:24:26 +0900

    DOMとイベントリスナにつて学ぼう

--- 03_js_basic/045_eventlistener/start/main.js
+++ 03_js_basic/045_eventlistener/start/main.js
@@ -8,7 +8,7 @@ h1Element.textContent = "変更後のタイトル";
 const btnEl = document.querySelector("button");
 btnEl.addEventListener("click", (e) => {
   //括弧内に入れるのはアクションのタイプ,実行したい関数
-  console.dir(e.target.textContent); //「dir」はDom要素が見れる（今回の場合<button>の要素）
+  console.dir(e.target.textContent); //「dir」はDom要素が見れる（今回の場合<button>の要素）,
 
   console.log("hello");
 });
