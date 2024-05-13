4cb1d0dd5da988e40d2fafec7f38796e50a9530e
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-14 08:59:07 +0900

    propsでコンポーネントに値を渡してみよう

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,10 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+  <>
+    <Child color="" />;
+    <Child color="Red" />;
+  </>;
+};
 
 export default Example;
