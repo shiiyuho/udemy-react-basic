7646dca701d2b1ef892da1a3c18911f06f976204
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-13 13:25:31 +0900

    12　ESModuleのExport/Importについて

--- 03_js_basic/030_esmodule/start/main.js
+++ 03_js_basic/030_esmodule/start/main.js
@@ -1,5 +1,5 @@
 import { hello, User } from "./module.js";
-import funcB from "./module.js"; //export defaultの場合は{}がいらない。
+import funcB from "./module.js"; //export defaultの場合は{}がいらない
 hello();
 const user = new User("Tom");
 user.hello();
