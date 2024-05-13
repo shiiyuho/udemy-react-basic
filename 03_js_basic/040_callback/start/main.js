e7c13b54aee647290d323bc66e66755c9757e23d
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-13 13:41:01 +0900

    コールバック関数の挙動について学ぼう

--- 03_js_basic/040_callback/start/main.js
+++ 03_js_basic/040_callback/start/main.js
@@ -1,15 +1,22 @@
-function fn(number) {
-    return number * 2;
-  }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+function print(fn) {
+  const result = fn(2);
+  console.log(result);
+}
+
+function fn(number = 3) {
+  return number * 2;
+}
+debugger; //「ここで止める」という意味
+print(fn);
+
+//   console.log(fn(2));
+//   console.log(fn(4));
+
+//   const fnArrow = number => {
+//     console.log(number);
+//     return number * 2;
+//   };
+
+//   const fnArrowObj = number => ({ result: number * 2 })
+
+//   console.log(fnArrowObj(2));
