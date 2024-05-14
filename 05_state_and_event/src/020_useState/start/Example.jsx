59b3c4af75aaf4079fed340fc64ebebc6e327f70
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-14 11:21:11 +0900

    イベントに合わせて画面表示を更新してみよう

--- 05_state_and_event/src/020_useState/start/Example.jsx
+++ 05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,10 +1,38 @@
-const Example = () => {
+//入力された値を画面上に出力(useState)
+import { useState } from "react";
 
+const Example = () => {
+  let valArry = useState(); //初期値セット
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          const setFn = valArry[1];
+          setFn(e.target.value);
+        }}
+      />
+      ={valArry[0]}
     </>
   );
 };
 
 export default Example;
+
+//分割代入の場合(useState)
+import { useState } from "react";
+
+const Example2 = () => {
+  let [val, setVal] = useState(); //初期値セット
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          setVal(e.target.value);
+        }}
+      />
+      ={val}
+    </>
+  );
+};
