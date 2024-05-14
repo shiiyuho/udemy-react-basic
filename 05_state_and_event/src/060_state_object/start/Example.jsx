496f015d0257573d534c73a0c1124b5bb49a5f7b
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-14 13:47:07 +0900

    オブジェクト型のステートを使う際の注意点！

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -1,7 +1,36 @@
+//プリミティブ型：1,str,bool,10n,Symbol(),null,undefined
+//オブジェクト型：{},[]などのプリミティブ型以外
 import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState({ name: "Tom", age: 18 });
+  //Name設定
+  const changeName = (e) => {
+    setPerson({ name: e.target.value, age: person.age });
+  };
+
+  //Age設定
+  const changeAge = (e) => {
+    setPerson({ name: person.name, age: e.target.value });
+  };
+
+  //リセット設定
+  const reset = () => {
+    setPerson({ name: "", age: "" });
+  };
+
+  return (
+    <>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.name}</h3>
+      <input type="text" value={person.name} onChange={changeName} />
+      <input type="number" value={person.age} onChange={changeAge} />
+      <div>
+        <button onClick={reset}></button>
+      </div>
+    </>
+  );
 };
 
 export default Example;
