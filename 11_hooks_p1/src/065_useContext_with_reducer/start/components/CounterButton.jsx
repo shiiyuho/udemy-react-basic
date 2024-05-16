9ef339eada278d8c2a5a56f17e90c4984f66067f
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-16 14:10:05 +0900

    useContextとuseReducerを組み合わせて使ってみよう

--- 11_hooks_p1/src/065_useContext_with_reducer/start/components/CounterButton.jsx
+++ 11_hooks_p1/src/065_useContext_with_reducer/start/components/CounterButton.jsx
@@ -1,5 +1,17 @@
-const CounterButton = ({calcType, step, onClick}) => {
-    
-    return <button onClick={onClick}>{calcType}{step}</button>
-}
+import { useCounter, useCounterDispatch } from "../context/CounterContext";
+
+const CounterButton = ({ calcType, step }) => {
+  const dispatch = useCounterDispatch();
+
+  const clickHandler = () => {
+    dispatchEvent({ type: calcType, step: step });
+  };
+
+  return (
+    <button onClick={clickHandler}>
+      {calcType}
+      {step}
+    </button>
+  );
+};
 export default CounterButton;
