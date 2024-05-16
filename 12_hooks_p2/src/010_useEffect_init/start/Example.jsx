868c8a63796d7d64b6ab29783ef50372b7e2f6d9
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-16 16:38:11 +0900

    useEffectとは？タイマーを作りながら学んでみよう

--- 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+  //配列でtimeを設定
+  const [time, setTime] = useState(0);
+
+  //useEffect(コンポーネントを1回だけ呼びたい時に使う)
+  useEffect(() => {
+    //下記によりコールバック関数が実行
+    window.setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      <h3>
+        <time datetime="">{time}</time>
+        <span>秒経過</span>
+      </h3>
+    </>
+  );
 };
 
 export default Example;
