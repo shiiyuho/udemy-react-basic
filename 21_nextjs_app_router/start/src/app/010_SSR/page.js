a8950fd574c423fc0c41642fe1cbe862bb7e5926
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-17 15:27:26 +0900

    SSRの挙動を理解しよう

--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,3 +1,17 @@
+"use Client";
+
+import { useEffect, useState } from "react";
+
 export default function SSR() {
-  return <div>SSR Page</div>;
+  const [state, useState] = useState(undefined);
+
+  useEffect(() => {
+    setState("client loaded");
+  }, []);
+  return (
+    <>
+      <div>SSR Page</div>
+      <div>{state}</div>
+    </>
+  );
 }
