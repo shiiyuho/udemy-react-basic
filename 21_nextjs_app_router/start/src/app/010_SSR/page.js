66892ee55d3e430ed2737b5bb154a8fa59e8105b
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-17 15:52:34 +0900

    APIでデータを取得して画面を構築してみよう

--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,18 +1,19 @@
 "use Client";
-
-import { useState, useEffect } from "react";
+import ArticleList from "@/components/articleList";
 import "./lib";
+import { ENDPOINT } from "@/constants";
+import ArticleList from "@/components/articleList";
 
-export default function SSR() {
-  const [state, setState] = useState(undefined);
-
-  useEffect(() => {
-    setState("client loaded");
-  }, []);
+export default async function SSR() {
+  const article = await featch(ENDPOINT, {
+    cache: "force-cache",
+  }).them((res) => res.json());
   return (
     <>
       <div>SSR Page</div>
       <div>{state}</div>
+      <ClientComp />
+      <ArticleList list={articles} basePath="/010_SSR" />
     </>
   );
 }
