d4bbbd84f18cdbad522b4250fe0f1e842f8d1b03
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-20 10:24:12 +0900

    API Routerでサーバーのデータを更新してみよう

--- 21_nextjs_app_router/end/src/app/api/article/route.js
+++ 21_nextjs_app_router/end/src/app/api/article/route.js
@@ -8,23 +8,29 @@ export async function GET() {
 }
 
 export async function POST(request) {
-  // const formData = await request.formData();
-  // const id = formData.get("id");
-  // const title = formData.get("title");
-  // if (id === "" || title === "") {
-  //   return Response.json({ msg: "入力フィールドが空です。" }, { status: 500 });
-  // }
-  // try {
-  //   const res = await fetch(ENDPOINT, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ id, title }),
-  //   });
-  //   const data = await res.json();
-  //   return Response.json(data);
-  // } catch (e) {
-  //   return Response.json({ msg: "登録に失敗しました。" }, { status: 500 });
-  // }
+  const formData = await request.formData();
+  //idとtitleに記入された値の出力
+  const id = formData.get("id");
+  const title = formData.get("title");
+
+  //idかtitleが空だった時にerrorを表示する（500番で）
+  if (id === "" || title === "") {
+    return Response.json({ msg: "入力フィールドが空です。" }, { status: 500 });
+  }
+  try {
+    //データがJsonサーバーに対して登録されるコード
+    const res = await fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, title }),
+    });
+    //jsonサーバーからけってきたJsonをレスポンスとしてAPIの戻り値にする
+    const data = await res.json();
+    return Response.json(data);
+    //エラーが起こったときに知らせる処理
+  } catch (e) {
+    return Response.json({ msg: "登録に失敗しました。" }, { status: 500 });
+  }
 }
